1565d499154459c87f84bf79c8a1928a42d5ff2e
Hoang Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2017-09-08 07:25:24 +0700

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectId('59b1dc26ae146e059b17a025')
+    // }).toArray().then(docs => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, err => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Todos').find().count().then(count => {
+        console.log(`Todos count: ${count}`);
+    }, err => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    db.collection('Users').find({
+        name: 'An'
+    }).toArray().then(docs => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, err => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    db.close();
+});;
