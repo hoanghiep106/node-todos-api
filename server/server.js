ae6fdcc833f04e2d88c91506aea958bb6471ed5b
Hoang Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2017-09-21 19:07:34 +0700

    Add POST users/login route

--- server/server.js
+++ server/server.js
@@ -96,6 +96,16 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    User.findByCredentials(req.body.email, req.body.password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch(e => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
